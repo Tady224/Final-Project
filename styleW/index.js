91b118d2cf29ed86c6f3a13ef63fc25dbaf9dd6d
Tady224 <78107107+Tady224@users.noreply.github.com>
2021-03-06 21:38:24 +0100

    add "last updated" + backround color

--- styleW/index.js
+++ styleW/index.js
@@ -1,44 +1,56 @@
+//Date
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
 
-let now = new Date();
-let h3 = document.querySelector("h3");
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-let hour = now.getHours();
-if (hour < 10) {
-  hour = `0:${hour}`;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
 }
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0:${minutes}`;
-}
-let day = days[now.getDay()];
-h3.innerHTML = `${day}, ${hour}:${minutes}`;
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+
+//CITY TEMP HUMIDITY WIND ICON
 function showWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#today").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round (response.data.wind.speed);
   document.querySelector("#icon").setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  document.querySelector("#date").innerHTML = formatDate(response.data.dt * 1000);
 }
-
+//API AXIOS
 function searchLocation(position) {
   let apiKey = "5625fc4e2f1e1a9677dec96db881138a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeather);
 }
-
+//Current Location
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
+//Add metric to API
 function search(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
